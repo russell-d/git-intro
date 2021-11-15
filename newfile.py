c31e4355c3b18c0abdd36d4c3d8999ea9d6f310b
russell-d <rdevera@csumb.edu>
2021-11-14 19:35:25 -0800

    hello

--- newfile.py
+++ newfile.py
@@ -1,6 +1,11 @@
 print("making stuff")
-<<<<<<< HEAD
 print("WOOO")
-=======
+
+
+
 print("ON NEW BRANCH! MAKING CHANGES!")
->>>>>>> 1aed592dbcf43303b205bed661ab269b16a9fa12
+
+
+
+
+print ("commit")
